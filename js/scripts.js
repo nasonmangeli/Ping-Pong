75ebf24780b24a9fba3900172e395b100881d6d9
nason mangeli <kuuchman@gmail.com>
2017-08-25 17:49:03 +0300

    Add functions 1. add for loop function to filter out ping, pong and pingpong 2. add .empty method to clear list 3. add for each loop to display outcomes in the outPutnumber array

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,22 @@ $(document).ready(function(){
   $("#numero").submit(function(event){
     var numberEntered = parseInt($("#number").val())
     var outPutnumber = [];
-    for(var index = 1; index <= numberEntered; index +=1){
-      alert(index);
-
-
-  };
+    for(var index = 1; index <= numberEntered; index ++){
+      if (index % 3 === 0 && index % 5 === 0) {
+        outPutnumber.push("pingpong")
+      } else if (index % 3 === 0) {
+        outPutnumber.push("ping")
+      } else if (index % 5 === 0) {
+        outPutnumber.push("pong")
+      } else {
+        outPutnumber.push(index)
+      }
+    };
+    console.log(outPutnumber);
+    $('ul.numberOutputs').empty();
+    outPutnumber.forEach(function (number) {
+      $('ul.numberOutputs').append('<li>'+number+'</li>');
+    });
     event.preventDefault();
   });
 });
