e417bc55fceac905c82a86fdfa6b090b5c8d785c
nason mangeli <kuuchman@gmail.com>
2017-08-27 15:47:18 +0300

    Separate Back-end from front-end logic 1. Create function for looping and assigning the string ping, pong and pingpong. Call it pingpongFilter 2. Call pingpongFilter and add forEach function to append in ul#output

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 //Back-end logic
-var outPuts = [];
 
+//PingPongfilter function with array as return
 var pingpongFilter = function(numberEntered){
+  var outPuts =[];
   for(var index = 1; index <= numberEntered; index += 1){
     if (index % 3 === 0 && index % 5 === 0) {
       outPuts.push("pingpong")
@@ -17,19 +18,27 @@ return outPuts;
 };
 
 
+
 //Front-end logic
 
 $(document).ready(function(){
   $("#form").submit(function(event){
+    event.preventDefault();
 
+    //Get value of numberEntered
     var numberEntered = parseInt($("#number").val());
 
-    pingpongFilter(numberEntered);
+    //Call the function pingpongFilter and append each element of its resulting array to ul with id="output" tag
+    pingpongFilter(numberEntered).forEach(function(number) {
+      $('#output').append('<li>'+number+'</li>')
+    })
 
-    outPuts.forEach(function (number) {
-      $('ul.numberOutputs').append('<li>'+number+'</li>');
-    });
+      // $('#number').val("");
+      // $('ul').empty();
+      // for (var i = 0; i < array.length; i++) {
+      //   $('ul').append('<li>'+output+'</li>')
+      // }
 
-    event.preventDefault();
+
   });
 });
